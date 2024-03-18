1c5e698aa9238ed581f01a119aa91597d151b8bb
Snow <142698082+itssherm@users.noreply.github.com>
2024-03-18 15:56:59 -0400

    Add files via upload

---
+++ HexedBase/Entry.cs
@@ -0,0 +1,49 @@
+﻿using CoreRuntime.Interfaces;
+using CoreRuntime.Manager;
+using Cysharp.Threading.Tasks.Linq;
+using System;
+using System.Collections;
+using UnityEngine;
+
+
+
+
+
+namespace HexedBase
+{
+    public class Entry : HexedCheat // Define the Main Class for the Loader
+    {
+        public override void OnLoad()
+        {
+            // Entry thats getting called by HexedLoader 
+            Console.WriteLine("Hexed Base Cheat successfully loaded!");
+            MonoManager.PatchUpdate(typeof(VRCApplication).GetMethod(nameof(VRCApplication.Update)));
+            main.OnApplicationStart();
+        }
+
+        public override void OnApplicationQuit()
+        {
+            Console.WriteLine("Game Closed! Bye!");
+        }
+
+        public override void OnUpdate()
+        {
+        }
+
+        public override void OnFixedUpdate()
+        {
+            // Function is not hooked, won't get called
+        }
+
+        public override void OnLateUpdate()
+        {
+            // Function is not hooked, won't get called
+        }
+
+        public override void OnGUI()
+        {
+            // Function is not hooked, won't get called
+        }
+
+    }
+}
---
+++ HexedBase/main.cs
@@ -0,0 +1,31 @@
+﻿using CoreRuntime.Manager;
+using Cysharp.Threading.Tasks.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HexedBase
+{
+    internal class main
+    {
+        public static void OnApplicationStart()
+        {
+            CoroutineManager.RunCoroutine(main.waitForQM());
+        }
+        private static IEnumerator waitForQM()
+        {
+            while (GameObject.Find("Canvas_QuickMenu(Clone)") == null) yield return null; // Trys to find the Qm, if the Find Return null, it will wait for the next frame and check
+            yield return null; // wait one more frame, this isn't needed, but shut up
+            Console.WriteLine("Menu loaded");
+            MakeMenu.MakeMainMenu();
+       
+           
+
+            yield break; // This stops the IEnum
+        }
+    }
+}
